5ca40a92afe0bf450d8187d6f3256aa39f689c65
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-11 19:46:25 +0900

    Initial commit

---
+++ src/main/java/com/kyonggi/Capstone_Develop/CapstoneDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.kyonggi.Capstone_Develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyonggi/Capstone_Develop/CapstoneDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyonggi.Capstone_Develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
