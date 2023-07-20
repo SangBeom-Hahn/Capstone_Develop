9a1a6f2e06bae081277665667801944f5ac7f9dc
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-20 17:39:14 +0900

    refactor: 회원, 관리자 도메인 멤버 value Object로 변경
    * feat: Email 클래스 기능 및 테스트 구현
    
    * feat: PhoneNumber 클래스 기능 및 테스트 구현

---
+++ src/test/java/com/kyonggi/Capstone_Develop/domain/student/EmailTest.java
@@ -0,0 +1,23 @@
+package com.kyonggi.Capstone_Develop.domain.student;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+    @Test
+    @DisplayName("잘못된 이메일 형식을 입력하면 예외가 발생한다.")
+    void throwException_invalidEmailFormat() {
+        assertThatThrownBy(() -> Email.from("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    @DisplayName("이메일 객체를 생성한다.")
+    void construct() {
+        assertDoesNotThrow(() -> Email.from("1@naver.com"));
+    }
+}
---
+++ src/test/java/com/kyonggi/Capstone_Develop/domain/student/PhoneNumberTest.java
@@ -0,0 +1,23 @@
+package com.kyonggi.Capstone_Develop.domain.student;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class PhoneNumberTest {
+    @Test
+    @DisplayName("잘못된 형식의 전화번호를 입력하면 예외가 발생한다.")
+    void throwException_invalidPhoneNumberFormat() {
+        assertThatThrownBy(() -> PhoneNumber.from("01011111111"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    @DisplayName("전화번호 객체를 생성한다.")
+    void construct() {
+        assertDoesNotThrow(() -> PhoneNumber.from("010-1111-1111"));
+    }
+}
