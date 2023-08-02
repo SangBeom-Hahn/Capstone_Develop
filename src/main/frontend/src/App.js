7cf0a9d9222dc2ca5380541cc89c3cd6b04b85e3
euna0104 <koeuna0104@naver.com>
2023-08-02 18:11:56 +0900

    feat: 초기화면 home으로 설정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import SignUp from './Signup';
+import Notice from './Notice';
 
 import './App.css';
 
@@ -10,12 +11,16 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/api/home" />} />
+
         {/* 홈 화면 */}
         <Route path="/api/home" element={<Home />} />
         {/* 로그인 화면 */}
         <Route path="/api/login" element={<Login />} />
         {/* 회원가입 화면 */}
         <Route path="/api/signup" element={<SignUp />} />
+        {/* 공지사항 화면 */}
+        <Route path="/api/notice" element={<Notice />} />
       </Routes>
     </Router>
   );
