6dbeb2b729447fbd3c0037c6d1ff1822efcbf27e
euna0104 <koeuna0104@naver.com>
2023-08-08 15:53:03 +0900

    feat: Header 구현

---
+++ src/main/frontend/src/Notice.jsx
@@ -0,0 +1,13 @@
+          import React from 'react';
+          import Header from './Header'; // Header 컴포넌트를 import
+
+          const Notice = () => {
+            return (
+              <div>
+                <Header /> {/* Header 컴포넌트 사용 */}
+                {/* 이후 Notice 컴포넌트의 내용 추가 */}
+              </div>
+            );
+          };
+
+          export default Notice;
