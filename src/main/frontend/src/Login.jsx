bc562d0f4ec9210af51aeec54f1899a3ef893038
EUN A <93309561+euna0104@users.noreply.github.com>
2023-08-15 15:48:14 +0900

    feat: 로그인 토큰 받아오는 기능 구현

--- src/main/frontend/src/Login.jsx
+++ src/main/frontend/src/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,20 +10,20 @@ const Login = () => {
     const handleLogin = async () => {
         try {
             const account = {
-                loginId: id,
-                loginPassword: password
-            };
+            loginId: id,
+            loginPassword: password
+        };
 
-            const response = await axios.post('/api/login/auth', account);
-            const url = response.data;
+        const response = await axios.post('/api/login/auth', account);
+        const token = response.data.token; // Assuming the token is returned in the response
 
-            alert('로그인 성공');
-            window.location.replace(url);
+        sessionStorage.setItem('token', token);
+
+        alert('로그인 성공');
         } catch (error) {
             alert(error.response.data.errorMessage);
         }
     };
-
 
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
@@ -36,9 +37,9 @@ const Login = () => {
                 <div className="container">
                     <div className="row g-0">
                         <div className="col-md-8 col-lg-7 col-xl-6 offset-md-2 offset-lg-2 offset-xl-3 space-top-3 space-lg-0">
-                            <a href="/api/home" className="d-flex justify-content-center align-items-center">
+                            <Link to="/api/home" className="d-flex justify-content-center align-items-center">
                                 <img src="/img/cspop_logo.png" alt="" width="130em" />
-                            </a>
+                            </Link>
                             <br />
                             <div className="bg-white p-4 p-xl-6 p-xxl-8 p-lg-4 rounded-3 border" style={{ maxWidth: "500px", margin: "0 auto" }}>
                                 <h1 className="mb-2 text-center h3">로그인</h1>
@@ -62,8 +63,8 @@ const Login = () => {
                                 </div>
                                 <div className="d-xxl-flex justify-content-end mt-4">
                                     <span className="text-muted font-15 mb-0">
-                                        <a href="/api/signup">회원가입</a>&nbsp;
-                                        <a href="/api/passwordReset">비밀번호변경</a>
+                                        <Link to="/api/signup">회원가입</Link>&nbsp;
+                                        <Link to="/api/passwordReset">비밀번호변경</Link>
                                     </span>
                                 </div>
                             </div>
