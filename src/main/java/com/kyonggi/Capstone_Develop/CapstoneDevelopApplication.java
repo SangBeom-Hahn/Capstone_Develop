e2088bf1729deceb6d4dff90a1a4489d63e16452
SangBeom-Hahn <hsb990917@gmail.com>
2023-08-15 14:28:55 +0900

    refactor: 스프링 config 계층 적용

--- src/main/java/com/kyonggi/Capstone_Develop/CapstoneDevelopApplication.java
+++ src/main/java/com/kyonggi/Capstone_Develop/CapstoneDevelopApplication.java
@@ -2,14 +2,12 @@ package com.kyonggi.Capstone_Develop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CapstoneDevelopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneDevelopApplication.class, args);
 	}
-
 }
