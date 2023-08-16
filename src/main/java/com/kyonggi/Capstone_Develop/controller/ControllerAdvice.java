fc870968b449c41c061d2c40ca28549dbe67ee00
SangBeom-Hahn <hsb990917@gmail.com>
2023-08-16 17:48:32 +0900

    feat: ControllerAdvice dto 예외 처리 추가

--- src/main/java/com/kyonggi/Capstone_Develop/controller/ControllerAdvice.java
+++ src/main/java/com/kyonggi/Capstone_Develop/controller/ControllerAdvice.java
@@ -3,16 +3,49 @@ package com.kyonggi.Capstone_Develop.controller;
 import com.kyonggi.Capstone_Develop.controller.dto.ErrorResponse;
 import com.kyonggi.Capstone_Develop.exception.CspopException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> handleInvalidRequest(
+            final BindingResult bindingResult,
+            final HttpServletRequest request,
+            final MethodArgumentNotValidException e
+    ) throws IOException {
+        final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        final FieldError mainError = fieldErrors.get(0);
+        final String[] errorInfo = Objects
+                .requireNonNull(mainError.getDefaultMessage())
+                .split(":");
+        
+        log.error("HandledException: {} {} statusCode={} errMessage={}\n",
+                request.getMethod(),
+                request.getRequestURI(),
+                HttpStatus.BAD_REQUEST.value(),
+                errorInfo[0]
+        );
+        
+        log.debug("Error StackTrace: ", e);
+        
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("0", errorInfo[0]));
+    }
+    
     @ExceptionHandler({CspopException.class})
     public ResponseEntity<ErrorResponse> handleCspopException(
             final CspopException e,
@@ -26,6 +59,8 @@ public class ControllerAdvice {
         );
         log.debug("Error StackTrace: ", e);
 
-        return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getErrorCode(), e.getShowMessage()));
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(new ErrorResponse(e.getErrorCode(), e.getShowMessage()));
     }
 }
