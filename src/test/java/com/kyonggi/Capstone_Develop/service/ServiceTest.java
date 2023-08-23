8de094521dbe315afaa32dee1c6552f9a8e59303
SangBeom-Hahn <hsb990917@gmail.com>
2023-08-23 17:01:54 +0900

    feat: 레포지토리, 서비스 추상화

---
+++ src/test/java/com/kyonggi/Capstone_Develop/service/ServiceTest.java
@@ -0,0 +1,30 @@
+package com.kyonggi.Capstone_Develop.service;
+
+import com.kyonggi.Capstone_Develop.repository.RefreshTokenRepository;
+import com.kyonggi.Capstone_Develop.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public abstract class ServiceTest {
+    @Autowired
+    protected NoticeBoardService noticeBoardService;
+    
+    @Autowired
+    protected StudentRepository studentRepository;
+    
+    @Autowired
+    protected AuthService authService;
+    
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    protected RefreshTokenRepository refreshTokenRepository;
+    
+    @Autowired
+    protected StudentService studentService;
+}
