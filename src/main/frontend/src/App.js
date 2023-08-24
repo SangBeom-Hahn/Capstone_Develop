141a218220a5abdcfe512022f803791a8f60e14f
euna0104 <koeuna0104@naver.com>
2023-08-24 18:11:32 +0900

    feat: 졸업자조회 뷰 생성

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,6 +6,7 @@ import SignUp from './Signup';
 import Notice from './Notice';
 import Guide from './Guide';
 import Schedule from './Schedule';
+import GraduationList from './GraduationList';
 
 import './App.css';
 
@@ -14,7 +15,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/api/home" />} />
-
         {/* 홈 화면 */}
         <Route path="/api/home" element={<Home />} />
         {/* 로그인 화면 */}
@@ -27,6 +27,8 @@ const App = () => {
         <Route path="/api/graduation/guide" element={<Guide />} />
         {/* 진행일정 화면 */}
         <Route path="/api/graduation/schedule" element={<Schedule />} />
+        {/* 졸업자 조회 화면 */}
+        <Route path="/api/graduation/graduate_management" element={<GraduationList />} />
       </Routes>
     </Router>
   );
