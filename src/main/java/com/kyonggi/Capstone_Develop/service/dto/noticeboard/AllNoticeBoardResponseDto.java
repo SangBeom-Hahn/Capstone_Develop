98538b5a9a6c2bfdd8622041f49c2594ed17d18a
SangBeom-Hahn <hsb990917@gmail.com>
2023-09-03 19:57:23 +0900

    fix: dto serialize 수정

--- src/main/java/com/kyonggi/Capstone_Develop/service/dto/noticeboard/AllNoticeBoardResponseDto.java
+++ src/main/java/com/kyonggi/Capstone_Develop/service/dto/noticeboard/AllNoticeBoardResponseDto.java
@@ -2,9 +2,11 @@ package com.kyonggi.Capstone_Develop.service.dto.noticeboard;
 
 import com.kyonggi.Capstone_Develop.domain.NoticeBoard;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @AllArgsConstructor
 public class AllNoticeBoardResponseDto {
     private Long id;
