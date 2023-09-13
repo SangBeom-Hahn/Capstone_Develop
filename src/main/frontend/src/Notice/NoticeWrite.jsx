3e10d45c10e1d107a465a7372f9eed6e1d1fe399
euna0104 <koeuna0104@naver.com>
2023-09-13 10:29:26 +0900

    fix: 공지사항 작성후 alert,페이지 전환 수정

--- src/main/frontend/src/Notice/NoticeWrite.jsx
+++ src/main/frontend/src/Notice/NoticeWrite.jsx
@@ -5,79 +5,78 @@ import CommonEditor from '.././Component/CommonEditor';
 import Header from '.././Header';
 
 const NoticeWrite = () => {
-    const [title, setTitle] = useState('');
-    const [contents, setContents] = useState('');
-    const [accessToken, setAccessToken] = useState('');
-    const [loggedInUserId, setLoggedInUserId] = useState('');
-    const currentDate = new Date().toLocaleDateString();
-    const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
+  const [accessToken, setAccessToken] = useState('');
+  const [loggedInUserId, setLoggedInUserId] = useState('');
+  const currentDate = new Date().toLocaleDateString();
+  const navigate = useNavigate();
 
-    const fetchLoggedInUserId = () => {
-        const user = sessionStorage.getItem('userId');
-        if (user) {
-            const userData = JSON.parse(user);
-            setLoggedInUserId(userData.userId);
-        }
-    };
-
-    useEffect(() => {
-        fetchLoggedInUserId();
-    }, []);
-
-    useEffect(() => {
-        const token = sessionStorage.getItem('accessToken');
-        setAccessToken(token);
-    }, []);
-
-    async function createNoticeBoard() {
-        try {
-            const views = 1;
-
-            const response = await axios.post('/api/admins/noticeboards', {
-                title: title,
-                content: contents,
-                fix: false,
-                views: views,
-            }, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                }
-            });
-            if (response.data && response.data.ok === 1) {
-                alert('저장 완료');
-            } else {
-                alert('게시물을 저장하는 도중 오류가 발생하였습니다.');
-            }
-        } catch (error) {
-            console.error(error);
-            alert('게시물을 저장하는 도중 오류가 발생하였습니다.');
-        }
+  useEffect(() => {
+    const user = sessionStorage.getItem('userId');
+    if (user) {
+      setLoggedInUserId(user);
     }
+  }, []);
 
-    function onEditorChange(value) {
-        setContents(value);
+  useEffect(() => {
+    const token = sessionStorage.getItem('accessToken');
+    setAccessToken(token);
+  }, []);
+
+  async function noticeWrite() {
+    try {
+      const views = 1;
+      const cleanedContents = contents.replace(/<p>/g, '').replace(/<\/p>/g, '');
+      const payload = await axios.post('/api/admins/noticeboards', {
+        title: title,
+        content: cleanedContents,
+        fix: false,
+        views: views,
+      }, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        }
+      });
+      if (payload.data && payload.data.id) {
+        const newNoticeId = payload.data.id;
+        console.log('게시물:'+ newNoticeId + ' 저장 완료');
+        alert('저장이 완료되었습니다.');
+        navigate('/api/noticeboards');
+      } else {
+        console.log('게시물 저장 실패');
+        alert('게시물을 저장하는 도중 오류가 발생하였습니다.');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('게시물을 저장하는 도중 오류가 발생하였습니다.');
     }
+  }
 
-    return (
-        <div>
-            <Header />
-            <div className="container" style={{ fontFamily: 'Noto Sans Korean, Malgun Gothic, sans-serif' }}>
-                <div className="lf-contents pd12">
-                    <div style={{ padding: '12px' }}>
-                        <div className="form-group">
-                            <input type="text" placeholder="제목" className="form-control" onChange={(event) => setTitle(event.target.value)} />
-                        </div>
-                        <CommonEditor value={contents} onChange={onEditorChange} />
-                        <div className="text-center pd12">
-                            <button className="lf-button primary" onClick={createNoticeBoard}>
-                                저장
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  function onEditorChange(value) {
+    setContents(value);
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className="container" style={{ fontFamily: 'Noto Sans Korean, Malgun Gothic, sans-serif' }}>
+        <div className="lf-contents pd12">
+          <div style={{ padding: '12px' }}>
+            <div className="form-group">
+              <input type="text" placeholder="제목" className="form-control" onChange={(event) => setTitle(event.target.value)} />
             </div>
+            <CommonEditor value={contents} onChange={onEditorChange} />
+            <div className="text-center pd12">
+              <button className="lf-button primary" onClick={noticeWrite}>
+                저장
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NoticeWrite;
