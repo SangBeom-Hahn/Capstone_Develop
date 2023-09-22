bf5c743c4e17bded5f3f40d578f83a81c065e5e1
euna0104 <koeuna0104@naver.com>
2023-09-23 01:36:03 +0900

    style: Header 경로 수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Home from './Home';
 import Login from './Login';
 import SignUp from './Signup';
-import Notice from './Notice';
+import NoticeList from './Notice/NoticeList';
+import NoticeView from './Notice/NoticeView';
+import NoticeWrite from './Notice/NoticeWrite';
+import NoticeUpdate from './Notice/NoticeUpdate';
 import Guide from './Guide';
-import ModifyGuide from './ModifyGuide';
 import Schedule from './Schedule';
 
 import './App.css';
@@ -15,7 +17,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/api/home" />} />
-
         {/* 홈 화면 */}
         <Route path="/api/home" element={<Home />} />
         {/* 로그인 화면 */}
@@ -23,11 +24,15 @@ const App = () => {
         {/* 회원가입 화면 */}
         <Route path="/api/Signup" element={<SignUp />} />
         {/* 공지사항 화면 */}
-        <Route path="/api/notice" element={<Notice />} />
+        <Route path="/api/noticeboards" element={<NoticeList />} />
+        {/* 공지사항 작성 화면 */}
+        <Route path="/api/admins/noticeboards" element={<NoticeWrite />} />
+        {/*공지사항 개당 화면 */}
+        <Route path="/api/noticeboards/:id" element={<NoticeView />} />
+        {/*공지사항 수정 화면 */}
+        <Route path="/api/admins/noticeboards/:id" element={<NoticeUpdate />} />
         {/* 안내 및 내규 화면 */}
         <Route path="/api/graduation/guide" element={<Guide />} />
-        {/* 안내 및 내규 수정 화면 */}
-        <Route path="/api/graduation/modifyGuide" element={<ModifyGuide />} />
         {/* 진행일정 화면 */}
         <Route path="/api/graduation/schedule" element={<Schedule />} />
       </Routes>
--- src/main/frontend/src/ModifyGuide.jsx
+++ src/main/frontend/src/ModifyGuide.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Header from './Header';
+import Header from './Common/Header';
 
 const ModifyGuide = ({ onSubmit, hasFieldErrors, getFieldError }) => {
   const [data, setData] = useState({ text: ''});
